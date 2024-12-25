6f871c1c7dc6e36be793331b0f9b07c6e859188d
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-12-25 23:06:16 +0530

    <Refac:*/*>?

--- app/src/main/java/net/runner/relley/fn/Login.kt
+++ app/src/main/java/net/runner/relley/fn/Login.kt
@@ -2,13 +2,13 @@ package net.runner.relley.fn
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import androidx.core.net.toUri
 import androidx.core.content.ContextCompat.startActivity
 import net.runner.relley.BuildConfig
 
 fun Redirect(context: Context){
-    val authUrl = "https://github.com/login/oauth/authorize?client_id=${BuildConfig.GITHUB_CLIENT_ID}&scope=repo&&redirect_uri=relley://callback"
-    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(authUrl))
+    val authUrl = "https://github.com/login/oauth/authorize?client_id=${BuildConfig.GITHUB_CLIENT_ID}&redirect_uri=relley://callback&scope=repo,user"
+    val intent = Intent(Intent.ACTION_VIEW, authUrl.toUri())
     startActivity(context, intent, null)
 
 }
