9a761aedd461a653b02de44d9a3c1288392a352a
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-12-25 23:08:40 +0530

    <Feat:Adds app redirection to download screen in navigation graph mapping,Adds intent to redirect for github app authorization Relley>?

--- app/src/main/java/net/runner/relley/MainActivity.kt
+++ app/src/main/java/net/runner/relley/MainActivity.kt
@@ -1,27 +1,35 @@
 package net.runner.relley
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import net.runner.relley.Data.getStoredAccessToken
 import net.runner.relley.Data.saveToken
+import net.runner.relley.Repository.Repository
+import net.runner.relley.Repository.redirectApp
 import net.runner.relley.Screens.Intermediate
 import net.runner.relley.Screens.Login
-import net.runner.relley.ViewModels.RepoViewModel
 import net.runner.relley.Screens.Main
-import net.runner.relley.fn.Repository
+import net.runner.relley.ViewModels.RepoViewModel
 import net.runner.relley.ui.theme.RelleyTheme
 
 class MainActivity : ComponentActivity() {
     private lateinit var repoViewModel: RepoViewModel
     private var repository : List<Repository>? = null
     private var avatar_url : String? = null
+    private var authorized: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,10 +54,15 @@ class MainActivity : ComponentActivity() {
         })
 
         val uri = intent?.data
-        Log.d("auth", "onCreateURI:$uri")
         uri?.getQueryParameter("code")?.let{code ->
-            Log.d("auth","onCreateCode:$code")
-            repoViewModel.getAccessToken(code)
+            if(getStoredAccessToken(this) == null) {
+                authorized=true
+                repoViewModel.getAccessToken(code)
+
+                val authUrl = "https://github.com/apps/rellley/installations/new"
+                val intent = Intent(Intent.ACTION_VIEW, authUrl.toUri())
+                ContextCompat.startActivity(this, intent, null)
+            }
         }
         enableEdgeToEdge()
         setContent {
@@ -60,13 +73,27 @@ class MainActivity : ComponentActivity() {
                     startDestination = "Intermediate"
                 ) {
                     composable(route = "InterMediate") {
-                        Intermediate(navController)
+                        Intermediate(navController,authorized)
                     }
                     composable(route = "Main") {
                         Main(navController,repository,avatar_url)
                     }
                     composable(route="Login") {
                         Login(navController)
+                    }
+                    composable(
+                        route="redirectApp/{repo}",
+                        arguments = listOf(
+                            navArgument("repo") {
+                                type = NavType.StringType
+                            }
+                        ) )
+                    {navBackStackEntry ->
+                        val repoId= navBackStackEntry.arguments?.getString("repo")
+                        val repo = repository?.filter { it.id == repoId?.toLong() }
+                        redirectApp(navController, repo!![0])
+
+
                     }
                 }
             }
